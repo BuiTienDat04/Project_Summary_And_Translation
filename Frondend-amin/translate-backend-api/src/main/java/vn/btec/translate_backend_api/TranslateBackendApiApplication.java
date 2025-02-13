bec1cca17415a9f669e74443a57e065ecc994c1e
Manhpro04 <Nducmanh181@gmail.com>
2025-02-13 18:09:59 +0700

    update backend api

---
+++ Frondend-amin/translate-backend-api/src/main/java/vn/btec/translate_backend_api/TranslateBackendApiApplication.java
@@ -0,0 +1,13 @@
+package vn.btec.translate_backend_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslateBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslateBackendApiApplication.class, args);
+	}
+
+}
---
+++ Frondend-amin/translate-backend-api/src/test/java/vn/btec/translate_backend_api/TranslateBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.btec.translate_backend_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslateBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
