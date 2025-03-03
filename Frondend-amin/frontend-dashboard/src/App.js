10efd2d629f2eb52fa05f082fe06e0ac1c2d9c3e
Hainam-17 <namct9922@gmail.com>
2025-03-03 15:20:14 +0700

    update-login-admin
    update login admin

--- Frondend-amin/frontend-dashboard/src/App.js
+++ Frondend-amin/frontend-dashboard/src/App.js
@@ -1,37 +1,18 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Navbar from "./components/Navbar";
 import UserManagement from "./components/UserManagement";
 import LoginPage from "./components/LoginPage";
 
 function App() {
-  const isAuthenticated = localStorage.getItem("adminToken"); // Kiểm tra token admin
-
   return (
     <div className="min-h-screen bg-gray-100">
       <Navbar />
       <Routes>
-      <Route
-  path="/"
-  element={<LoginPage onLoginSuccess={(user) => {
-    if (user.role === "admin") {
-      localStorage.setItem("adminToken", "your_admin_token_here"); // Giả lập token admin
-      window.location.href = "/dashboard"; // Chuyển hướng sau khi login thành công
-    } else {
-      alert("Bạn không có quyền truy cập vào trang admin.");
-    }
-  }} />}
-/>
-
-        <Route
-          path="/dashboard"
-          element={isAuthenticated ? <Dashboard /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/user-management"
-          element={isAuthenticated ? <UserManagement /> : <Navigate to="/" />}
-        />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/user-management" element={<UserManagement />} />
       </Routes>
     </div>
   );
