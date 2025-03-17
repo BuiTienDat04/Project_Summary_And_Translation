4932f0b5d16a7c3ea1ec0583b9ce2c2d910d765c
Hainam-17 <namct9922@gmail.com>
2025-03-17 12:46:33 +0700

    update-fr-admin

--- Frondend-amin/frontend-dashboard/src/App.js
+++ Frondend-amin/frontend-dashboard/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Navbar from "./components/Navbar";
+import LoginPage from "./components/LoginPage";
 import UserManagement from "./components/UserManagement";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/user-management" element={<UserManagement />} />
       </Routes>
     </div>
