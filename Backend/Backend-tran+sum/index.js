51e980b8d6fc5ac9eaeb175414f8f7dd8b240c08
BuiTienDat04 <142377956+BuiTienDat04@users.noreply.github.com>
2025-04-01 03:04:49 +0700

    Update index.js
    sdsd

--- Backend/Backend-tran+sum/index.js
+++ Backend/Backend-tran+sum/index.js
@@ -436,14 +436,19 @@ connectDB().then(() => {
     // Tạo server HTTP từ ứng dụng Express
     server = http.createServer(app);
 
-    // Khởi tạo Socket.IO và gắn vào server
     const io = new Server(server, {
         cors: {
-            origin: ["http://localhost:3000", "http://localhost:3001", "https://pdfsmart.online, https://admin.pdfsmart.online"],
+            origin: [
+                "http://localhost:3000",
+                "http://localhost:3001",
+                "https://pdfsmart.online",
+                "https://admin.pdfsmart.online"
+            ],
             methods: ["GET", "POST"],
             credentials: true,
         },
     });
+    
 
     // Khởi động server
     server.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
