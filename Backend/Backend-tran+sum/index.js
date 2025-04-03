fc28528636a0c9259df206b52a19b0161b5d3463
Manhpro04 <166919626+Manhpro04@users.noreply.github.com>
2025-04-04 03:35:40 +0700

    Update index.js

--- Backend/Backend-tran+sum/index.js
+++ Backend/Backend-tran+sum/index.js
@@ -54,7 +54,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const cors = require("cors");
 app.use(cors({
-    origin: ["http://localhost:3000", "http://localhost:3001", "https://pdfsmart.online"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "https://pdfsmart.online","https://admin.pdfsmart.online" ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Set-Cookie"],
